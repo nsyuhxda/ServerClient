d4f0b400e78dfac654f1585a9594c10e4855bacd
nsyuhxda <48403898+nsyuhxda@users.noreply.github.com>
2019-03-20 17:03:22 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,61 +1,59 @@
-import java.net.*; 
-import java.io.*; 
-  
-public class Server 
-{ 
-    //initialize socket and input stream 
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-  
-    // constructor with port 
-    public Server(int port) 
-    { 
-        // starts server and waits for a connection 
-        try
-        { 
-            server = new ServerSocket(port); 
-            System.out.println("Server begin"); 
-  
-            System.out.println("Waiting for connection from client"); 
-  
-            socket = server.accept(); 
-            System.out.println("Successfully connected"); 
-  
-            // takes input from the client socket 
-            in = new DataInputStream( 
-                new BufferedInputStream(socket.getInputStream())); 
-  
-            String line = ""; 
-  
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) 
-            { 
-                try
-                { 
-                    line = in.readUTF(); 
-                    System.out.println(line); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("Connection closed"); 
-  
-            // close connection 
-            socket.close(); 
-            in.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Server server = new Server(5678); 
-    } 
-} 
+import java.io.*;
+
+import java.net.*;
+
+class client1
+
+{
+
+  public static void main(String[] args) throws Exception
+
+  {
+
+     Socket sock = new Socket("192.168.40.132", 9706);
+
+                               // reading from keyboard (keyRead object)
+
+     BufferedReader keyRead = new BufferedReader(new 
+InputStreamReader(System.in));
+
+                              // sending to client (pwrite object)
+
+     OutputStream ostream = sock.getOutputStream(); 
+
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+
+
+
+                              // receiving from server ( receiveRead  object)
+
+     InputStream istream = sock.getInputStream();
+
+     BufferedReader receiveRead = new BufferedReader(new 
+InputStreamReader(istream));
+
+
+
+     System.out.println("WELCOME:)");
+
+
+
+     String receiveMessage, sendMessage;               
+
+     while(true)
+
+     {
+
+        sendMessage = keyRead.readLine();  // keyboard reading
+
+        pwrite.println(sendMessage);       // sending to server
+
+        pwrite.flush();
+	
+	if((receiveMessage = receiveRead.readLine()) != null)
+	{
+		System.out.println("Server: " + receiveMessage);
+	}
+}
+}
+}
